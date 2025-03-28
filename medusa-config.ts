6a526df0f076638ac464850ef843a7a5a5e6b3d3
Francis Craven <craven.francis@gmail.com>
2025-03-28 12:33:58 -0400

    added sendgrid to medusa.config

--- medusa-config.ts
+++ medusa-config.ts
@@ -85,6 +85,24 @@ module.exports = defineConfig({
         ]
       }
     },
+    //SendGrid Notifications
+    {
+      resolve: "@medusajs/medusa/notification",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/notification-sendgrid",
+            id: "sendgrid",
+            options: {
+              channels: ["email"],
+              api_key: process.env.SENDGRID_API_KEY,
+              from: process.env.SENDGRID_FROM,
+            },
+          },
+        ],
+      },
+    },
     // Additional architectural modules can be added here
   ],
 }); 
+
