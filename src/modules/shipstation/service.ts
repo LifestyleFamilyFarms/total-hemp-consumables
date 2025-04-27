f7df5fba2364734705851a806c8dae1e506ff55f
Francis Craven <craven.francis@gmail.com>
2025-04-27 16:25:15 -0400

    getFulfillmentOptions method added to ShipStationProviderService

--- src/modules/shipstation/service.ts
+++ src/modules/shipstation/service.ts
@@ -1,5 +1,6 @@
 import { AbstractFulfillmentProviderService } from "@medusajs/framework/utils"
 import { ShipStationClient } from './client'
+import { FulfillmentOption } from "@medusajs/framework/types"
 
 export type ShipStationOptions = {
   api_key: string
@@ -15,6 +16,25 @@ class ShipStationProviderService extends AbstractFulfillmentProviderService {
 
     this.options_ = options
     this.client = new ShipStationClient(options)
+  }
+
+  async getFulfillmentOptions(): Promise<FulfillmentOptions[]> {
+    const { carriers } = await this.client.getCarriers()
+    const fulfillmentOptions: FulfillmentOption[] = []
+
+    carriers.filter((carrier) => !carrier.disabled_by_billing_plan)
+    .forEach((carrier) => {
+      carrier.services.forEach((service) => {
+        fulfillmentOptions.push({
+          id: `${carrier.carrier_id}__${service.service_code}`,
+          name: service.name,
+          carrier_id: carrier.carrier_id,
+          carrier_service_code: service.service_code
+        })
+      })
+    })
+
+    return fulfillmentOptions
   }
 
   // TODO add methods
