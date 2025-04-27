5332b67c691cc94315d0f0cac89c8005ab1674b9
Francis Craven <craven.francis@gmail.com>
2025-04-27 17:19:07 -0400

    createShipment added to ShipStationProviderService

--- src/modules/shipstation/service.ts
+++ src/modules/shipstation/service.ts
@@ -1,9 +1,17 @@
-import { AbstractFulfillmentProviderService } from "@medusajs/framework/utils"
-import { ShipStationClient } from './client'
 import { 
+  AbstractFulfillmentProviderService,
+  MedusaError
+} from "@medusajs/framework/utils"
+import { 
+  CalculateShippingOptionPriceDTO,
+  CreateShippingOptionDTO,
   FulfillmentOption,
-  CreateShippingOptionDTO
- } from "@medusajs/framework/types"
+} from "@medusajs/framework/types"
+import { 
+  GetShippingRatesResponse,
+  ShipStationAddress,
+  ShipStationClient 
+} from './client'
 
 export type ShipStationOptions = {
   api_key: string
@@ -42,6 +50,65 @@ class ShipStationProviderService extends AbstractFulfillmentProviderService {
 
   async canCalculate(data: CreateShippingOptionDTO): Promise<boolean> {
     return true
+  }
+
+  private async createShipment({
+    carrier_id,
+    carrier_service_code,
+    from_address,
+    to_address,
+    items,
+    currency_code,
+  }: {
+    carrier_id: string
+    carrier_service_code: string
+    from_address?: {
+      name?: string
+      address?: Omit<
+        StockLocationAddressDTO, "created_at" | "updated_at" | "deleted_at"
+      >
+    },
+    to_address?: Omit<
+      CartAddressDTO, "created_at" | "updated_at" | "deleted_at" | "id"
+    >,
+    items: CartLineItemDTO[] | OrderLineItemDTO[],
+    currency_code: string
+  }): Promise<GetShippingRatesResponse> {
+    if (!from_address?.address) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "from_location.address is required to calculate shipping rate"
+      )
+    }
+
+    const ship_from: ShipStationAddress = {
+      name: from_address?.name || "",
+      phone: from_address?.address?.phone || "",
+      address_line1: from_address?.address?.address_1 || "",
+      city_locality: from_address?.address?.city || "",
+      state_province: from_address?.address?.province || "",
+      postal_code: from_address?.address?.postal_code || "",
+      country_code: from_address?.address?.country_code || "",
+      address_residential_indicator: "unknown",
+    }
+    if (!to_address) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "shipping_address is required to calculate shipping rate"
+      )
+    }
+
+    const ship_to: ShipStationAddress = {
+      name: `${to_address.first_name} ${to_address.last_name}`,
+      phone: to_address.phone || "",
+      address_line1: to_address.address_1 || "",
+      city_locality: to_address.city || "",
+      state_province: to_address.province || "",
+      postal_code: to_address.postal_code || "",
+      country_code: to_address.country_code || "",
+      address_residential_indicator: "unknown",
+    }
+    // TODO create shipment
   }
 
   // TODO add methods
