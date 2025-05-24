eacb9366d0ab55ab4a5354c31d2dfe970df33fb5
Francis Craven <craven.francis@gmail.com>
2025-05-24 16:38:46 -0400

    removing sanity subscriber

--- src/subscribers/sanity-product-sync.ts
+++ src/subscribers/sanity-product-sync.ts
@@ -1,23 +1,23 @@
-import type {
-    SubscriberArgs,
-    SubscriberConfig,
-} from "@medusajs/medusa"
+// import type {
+//     SubscriberArgs,
+//     SubscriberConfig,
+// } from "@medusajs/medusa"
 
-import {
-    sanitySyncProductsWorkflow,
-} from '../workflows/sanity-sync-products'
+// import {
+//     sanitySyncProductsWorkflow,
+// } from '../workflows/sanity-sync-products'
 
-export default async function upsertSanityProduct({
-    event: { data },
-    container,
-}: SubscriberArgs<{id: string }>) {
-    await sanitySyncProductsWorkflow(container).run({
-        input: {
-            product_ids: [data.id],
-        }
-    })
-}
+// export default async function upsertSanityProduct({
+//     event: { data },
+//     container,
+// }: SubscriberArgs<{id: string }>) {
+//     await sanitySyncProductsWorkflow(container).run({
+//         input: {
+//             product_ids: [data.id],
+//         }
+//     })
+// }
 
-export const config: SubscriberConfig = {
-    event: ["product.created", "product.updated"],
-}
+// export const config: SubscriberConfig = {
+//     event: ["product.created", "product.updated"],
+// }
